406376900e4802927cb7989a7ffadec7ed57ec6e
Xavier Tô <xavier.to.01@gmail.com>
2011-08-08 12:13:45 -0400

    Inches work

--- lib/length_converter.rb
+++ lib/length_converter.rb
@@ -1,3 +1,9 @@
 class LengthConverter
-
+  def self.convert decimal_form
+    if decimal_form == 1
+      "1 inch"
+    else
+      "#{decimal_form} inches"
+    end
+  end
 end
--- spec/length_converter_spec.rb
+++ spec/length_converter_spec.rb
@@ -2,7 +2,15 @@ require "spec_helper"
 
 describe LengthConverter do
 
-  it "should do something" do
-    fail
+  it "should convert 1 to 1 inch" do
+    LengthConverter.convert(1).should == "1 inch"
+  end
+
+  it "should convert 2 to 2 inches" do
+    LengthConverter.convert(2).should == "2 inches"
+  end
+
+  it "should convert 8 to 8 inches" do
+    LengthConverter.convert(8).should == "8 inches"
   end
 end
